3f7e661e48fb43ce631180a0cf2c73cd85507dfe
Avleen Vig <avig@twilio.com>
2024-10-23 23:21:27 +0100

    Download 1% of rows from the last day, not just a fixed number

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 
 	"cloud.google.com/go/bigquery"
 	_ "github.com/marcboeker/go-duckdb"
@@ -71,8 +72,21 @@ func listTables(ctx context.Context, client *bigquery.Client, datasetID string) 
 }
 
 func downloadTableData(ctx context.Context, client *bigquery.Client, datasetID, tableID string, db *sql.DB) error {
-	fmt.Printf("Downloading data for table %s\n", tableID)
-	query := fmt.Sprintf("SELECT message FROM `%s.%s.%s` LIMIT 100000", client.Project(), datasetID, tableID)
+	rowCount, err := countRows(ctx, client, datasetID, tableID)
+	if err != nil {
+		return err
+	}
+	if rowCount == 0 {
+		fmt.Printf("No data found for table %s\n", tableID)
+		return nil
+	}
+	fmt.Printf("Table %s has %d rows\n", tableID, rowCount)
+
+	// Calculate 1% of the number of rows, rounded up to the nearest integer
+	sampleSize := int(math.Ceil(float64(rowCount) * 0.01))
+
+	fmt.Printf("Downloading %d rows for table %s\n", sampleSize, tableID)
+	query := fmt.Sprintf("SELECT message FROM `%s.%s.%s` LIMIT %d", client.Project(), datasetID, tableID, sampleSize)
 	q := client.Query(query)
 	it, err := q.Read(ctx)
 	if err != nil {
@@ -97,6 +111,35 @@ func downloadTableData(ctx context.Context, client *bigquery.Client, datasetID, 
 	}
 
 	return nil
+}
+
+func countRows(ctx context.Context, client *bigquery.Client, datasetID string, tableID string) (int, error) {
+	fmt.Println("Counting rows in the last 24 hours")
+	query := fmt.Sprintf(
+		"SELECT COUNT(*) FROM `%s.%s.%s` WHERE message IS NOT NULL AND timestamp >= TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 1 DAY)",
+		client.Project(),
+		datasetID,
+		tableID,
+	)
+	q := client.Query(query)
+	it, err := q.Read(ctx)
+	if err != nil {
+		return 0, err
+	}
+	// Return the number of rows
+	var count int
+	for {
+		var values []bigquery.Value
+		err := it.Next(&values)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+		count = int(values[0].(int64))
+	}
+	return count, nil
 }
 
 func saveToDuckDB(db *sql.DB, tableName, message string) error {
